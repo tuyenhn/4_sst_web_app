2e75e8037d65a178119572cfa92dbcff46dc5bac
tuyenhn <hntuyen282@gmail.com>
2020-12-09 22:59:28 +0700

    refactor js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,40 +1,32 @@
+// carousel
 let cont = 0;
-let xx;
 
-let switchSlide = (slider1, slider2, button1, button2) => {
+let switchSlide = (slider1, slider2) => {
   $(slider1).fadeOut(400);
   $(slider2).delay(400).fadeIn(400);
-  $(button1).removeClass("bg-purple-800");
-  $(button2).addClass("bg-purple-800");
 };
 
 let loopSlider = () => {
-  xx = setInterval(() => {
+  setInterval(() => {
     switch (cont) {
       case 0: {
-        switchSlide("#slider-1", "#slider-2", "#sButton1", "#sButton2");
+        switchSlide("#slider-1", "#slider-2");
         cont = 1;
 
         break;
       }
       case 1: {
-        switchSlide("#slider-2", "#slider-1", "#sButton2", "#sButton1");
+        switchSlide("#slider-2", "#slider-1");
         cont = 0;
 
         break;
       }
     }
-  }, 8000);
-};
-
-let reinitLoop = (time) => {
-  clearInterval(xx);
-  setTimeout(loopSlider(), time);
+  }, 3000);
 };
 
 $(window).ready(() => {
   $("#slider-2").hide();
-  $("#sButton1").addClass("bg-purple-800");
 
   loopSlider();
 });
