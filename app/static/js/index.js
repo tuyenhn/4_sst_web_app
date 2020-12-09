673b853300f75db3a7dc56e64933fcaa9b576832
tuyenhn <hntuyen282@gmail.com>
2020-12-09 19:52:57 +0700

    refactor js, remove carousel buttons

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,54 +1,38 @@
-var cont = 0;
-function loopSlider() {
-  var xx = setInterval(function () {
+let cont = 0;
+let xx;
+
+let switchSlide = (slider1, slider2, button1, button2) => {
+  $(slider1).fadeOut(400);
+  $(slider2).delay(400).fadeIn(400);
+  $(button1).removeClass("bg-purple-800");
+  $(button2).addClass("bg-purple-800");
+};
+
+let loopSlider = () => {
+  xx = setInterval(() => {
     switch (cont) {
       case 0: {
-        $("#slider-1").fadeOut(400);
-        $("#slider-2").delay(400).fadeIn(400);
-        $("#sButton1").removeClass("bg-purple-800");
-        $("#sButton2").addClass("bg-purple-800");
+        switchSlide("#slider-1", "#slider-2", "#sButton1", "#sButton2");
         cont = 1;
 
         break;
       }
       case 1: {
-        $("#slider-2").fadeOut(400);
-        $("#slider-1").delay(400).fadeIn(400);
-        $("#sButton2").removeClass("bg-purple-800");
-        $("#sButton1").addClass("bg-purple-800");
-
+        switchSlide("#slider-2", "#slider-1", "#sButton2", "#sButton1");
         cont = 0;
 
         break;
       }
     }
   }, 8000);
-}
+};
 
-function reinitLoop(time) {
+let reinitLoop = (time) => {
   clearInterval(xx);
   setTimeout(loopSlider(), time);
-}
+};
 
-function sliderButton1() {
-  $("#slider-2").fadeOut(400);
-  $("#slider-1").delay(400).fadeIn(400);
-  $("#sButton2").removeClass("bg-purple-800");
-  $("#sButton1").addClass("bg-purple-800");
-  reinitLoop(4000);
-  cont = 0;
-}
-
-function sliderButton2() {
-  $("#slider-1").fadeOut(400);
-  $("#slider-2").delay(400).fadeIn(400);
-  $("#sButton1").removeClass("bg-purple-800");
-  $("#sButton2").addClass("bg-purple-800");
-  reinitLoop(4000);
-  cont = 1;
-}
-
-$(window).ready(function () {
+$(window).ready(() => {
   $("#slider-2").hide();
   $("#sButton1").addClass("bg-purple-800");
 
