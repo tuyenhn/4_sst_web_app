4b59494ea2decf331df256b3623185a0514fc261
tuyenhn <hntuyen282@gmail.com>
2020-12-10 15:49:15 +0700

    added fontawesome and relative path

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -25,6 +25,36 @@ let loopSlider = () => {
   }, 3000);
 };
 
+// Caps-lock warning
+let text = $("#capsWarning");
+text.addClass("text-transparent");
+
+$(window).keyup((event) => {
+  if (event.originalEvent.getModifierState("CapsLock")) {
+    text.removeClass("text-transparent");
+  } else {
+    text.addClass("text-transparent");
+  }
+});
+
+// Password visibility
+
+let pwd = $("#password");
+let eye = $("#eye");
+$(() => {
+  $('#eyeBtn').click(() => {
+    if (eye.hasClass('fa-eye-slash')) {
+      eye.removeClass('fa-eye-slash');
+      eye.addClass('fa-eye');
+      pwd.attr('type', 'text');
+    } else {
+      eye.removeClass('fa-eye');
+      eye.addClass('fa-eye-slash');
+      pwd.attr('type', 'password');
+    }
+  });
+});
+
 $(document).ready(() => {
   $(".no-fouc").removeClass('no-fouc');
 })
