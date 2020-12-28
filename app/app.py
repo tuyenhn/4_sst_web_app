77e2be7042eb63d9ed48dc36733cd6b7970166b2
tuyenhn <hntuyen282@gmail.com>
2020-12-28 23:53:14 +0700

    refactor

--- app/app.py
+++ app/app.py
@@ -5,7 +5,8 @@ from flask import Flask, render_template, session, request, redirect
 
 dirname = path.dirname(__file__)
 certpath = path.join(dirname, "cert/")
-languageJson = load(open(dirname + "/static/language.json"))
+langFile = open(path.join(dirname, "static/language.json"), encoding="utf-8")
+languageJson = load(langFile)
 
 app = Flask(__name__)
 app.secret_key = urandom(24)
@@ -13,53 +14,53 @@ app.secret_key = urandom(24)
 
 @app.before_first_request
 def firstSetup():
-	session["theme"] = ""
-	session["darkBtnInp"] = ""
-	session["lang"] = "English"
-	setLang(session["lang"])
+    session["theme"] = ""
+    session["darkBtnInp"] = ""
+    session["lang"] = "English"
+    setLang(session["lang"])
 
 
 @app.route("/")
 def home():
-	return render_template("home.html")
+    return render_template("home.html")
 
 
 @app.route("/paint")
 def paint():
-	return render_template("paint.html")
+    return render_template("paint.html")
 
 
 @app.route("/settings")
 def settings():
-	return render_template("settings.html")
+    return render_template("settings.html")
 
 
 @app.route("/help")
 def help():
-	return render_template("help.html")
+    return render_template("help.html")
 
 
 def setLang(lang):
-	session["selectedLang"] = languageJson["language"][lang]
+    session["selectedLang"] = languageJson["language"][lang]
 
 
 @app.route("/applySettings", methods=["post"])
 def applySettings():
-	try:
-		_ = request.form["darkModeInp"]
-		session["theme"] = "dark"
-		session["darkBtnInp"] = "checked"
-	except KeyError:
-		session["theme"] = ""
-		session["darkBtnInp"] = ""
+    try:
+        _ = request.form["darkModeInp"]
+        session["theme"] = "dark"
+        session["darkBtnInp"] = "checked"
+    except KeyError:
+        session["theme"] = ""
+        session["darkBtnInp"] = ""
 
-	userLang = request.form["languageInp"]
+    userLang = request.form["languageInp"]
 
-	if userLang != session["lang"]:
-		session["lang"] = userLang
-		setLang(userLang)
+    if userLang != session["lang"]:
+        session["lang"] = userLang
+        setLang(userLang)
 
-	return redirect("/settings")
+    return redirect("/settings")
 
 
 app.config["SERVER_NAME"] = "4sst.rmit:443"
